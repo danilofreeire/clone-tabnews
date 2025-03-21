82eaa70b8688a20bb6cc6809356a3daa2d84fb9d
Danilo Freire <154559134+danilofreeire@users.noreply.github.com>
2025-03-21 14:53:46 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1>Bia, eu te amo.❤️ Você é uma pessoa incrível e especial!</h1>
+function Home() {
+  return <h1>Bia, eu te amo.❤️ Você é uma pessoa incrível e especial!</h1>;
+}
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  cancelIdleCallbackonsole.log("teste");
 }
 
 export default Home;
