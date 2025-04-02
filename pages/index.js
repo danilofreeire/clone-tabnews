4fe0ece6b265b2efd57117a3e3f072818f8ef485
Danilo Freire <154559134+danilofreeire@users.noreply.github.com>
2025-04-02 14:48:12 +0000

    add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
   return <h1>Bia, eu te amo.❤️ Você é uma pessoa incrível e especial!</h1>;
 }
-function teste() {
-  console.log("teste");
-}
-
-function teste() {
-  cancelIdleCallbackonsole.log("teste");
-}
 
 export default Home;
