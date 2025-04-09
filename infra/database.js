886eb5be831a67d526031c13b8589d6ee2410ad7
Danilo Freire <154559134+danilofreeire@users.noreply.github.com>
2025-04-09 23:04:30 +0000

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,22 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   }); //comando síncrono
-  await client.connect(); //comando assíncrono, precisa ir no banco para se conectar de fato
+
+  console.log("Credenciais do postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect(); //comando assíncrono, precisa ir no banco para se conectar de fato
     const result = await client.query(queryObject);
     return result;
   } catch (err) {
     console.error(err);
+    throw err;
   } finally {
     await client.end();
   }
