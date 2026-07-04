9918cbb461562262d054b0cd348b568c87286e7b
Danilo Freire <danilofreirea90@gmail.com>
2026-07-04 09:51:52 -0300

    refactor: make `/api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,6 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database";
 import controller from "infra/controller";
+import migrator from "models/migrator";
 
 const router = createRouter();
 
@@ -11,32 +9,13 @@ router.post(postHandler);
 
 export default router.handler(controller.errorHandlers);
 
-async function runMigrations(options = {}) {
-  const dbClient = await database.getNewClient();
-
-  try {
-    const result = await migrationRunner({
-      dbClient,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-      ...options,
-    });
-
-    return result;
-  } finally {
-    await dbClient.end();
-  }
-}
-
 async function getHandler(request, response) {
-  const pendingMigrations = await runMigrations({ dryRun: true });
+  const pendingMigrations = await migrator.listPendingMigrations();
   return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  const migratedMigrations = await runMigrations({ dryRun: false });
+  const migratedMigrations = await migrator.runPendingMigrations();
 
   if (migratedMigrations.length > 0) {
     return response.status(201).json(migratedMigrations);
